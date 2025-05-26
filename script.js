a7a6437a80fc27bcf7ed30519447e11bfcb10b61
zah1rr <137958877+zah1rr@users.noreply.github.com>
2025-05-26 18:17:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var lastScrollTop = 0;
+navbar = document.getElementById("nav");
+window.addEventListener("scroll", function () {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-180px";
+  } else {
+    navbar.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
+  
+});
+
+
